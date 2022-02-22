3d827477e1437c8dd49bbb474c24221a941a11ed
Vinny D <vdurham@prideseals.com>
2022-02-21 23:30:34 -0500

    local storage setup

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,20 @@
-var today = moment().format("dddd, MMMM Do YYYY")
+var today = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(today);
+
+$(".time-block").each(function() {
+    var id = $(this).attr("id");
+    var save = localStorage.getItem(id);
+    
+    if (save !== null) {
+        $(this).children(".description").val(save);
+    }
+})
+
+$(".saveBtn").on("click", function() {
+    var text = $(this).siblings(".description").val();
+    console.log(text);
+    var time = $(this).parent().attr("id");
+    console.log(time, text);
+    localStorage.setItem(time, text);
+});
 
