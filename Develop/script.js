27466858079df7cbda4502ae02329cbc55cf4979
Vinny D <vdurham@prideseals.com>
2022-02-21 23:44:13 -0500

    added color effects with time

--- Develop/script.js
+++ Develop/script.js
@@ -18,3 +18,24 @@ $(".saveBtn").on("click", function() {
     localStorage.setItem(time, text);
 });
 
+var background = function() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function() {
+        var timeId = parseInt($(this).attr("id"));
+        if (timeId < currentHour) {
+            $(this).addClass("past");
+        }
+        else if (timeId === currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+}   
+
+background()
